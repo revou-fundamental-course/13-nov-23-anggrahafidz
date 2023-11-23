531841036b521f3c06aef34c8bad7634ef816d4f
anggrahafidz <77824224+anggrahafidz@users.noreply.github.com>
2023-11-23 22:56:10 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,68 @@
+function validateForm() {
+  let nameContactName =
+    document.forms["form-contact-us"]["nameContactName"].value;
+  let nameContactEmail =
+    document.forms["form-contact-us"]["nameContactEmail"].value;
+  let nameContactInterested =
+    document.forms["form-contact-us"]["nameContactInterested"].value;
+
+  if (nameContactName == "") {
+    alert("Name must be filled out");
+    return false;
+  } else if (nameContactEmail == "") {
+    alert("Email must be filled out");
+    return false;
+  } else if (nameContactInterested == "") {
+    alert("Your Interested must be filled out");
+    return false;
+  } else {
+    document.forms["form-contact-us"]["nameContactName"].value = "";
+    document.forms["form-contact-us"]["nameContactEmail"].value = "";
+    document.forms["form-contact-us"]["nameContactInterested"].value = "";
+    alert("Berhasil!");
+    return false;
+  }
+}
+
+let slideIndex = 0;
+function showSlides() {
+  let slides = document.getElementsByClassName("my-slides");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+let slideIndexPack = 0;
+function showSlidesPack() {
+  let slides = document.getElementsByClassName("our-package-slides");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndexPack++;
+  if (slideIndexPack > slides.length) {
+    slideIndexPack = 1;
+  }
+
+  slides[slideIndexPack - 1].style.display = "block";
+  setTimeout(showSlidesPack, 2000); // Change image every 2 seconds
+}
+
+showSlides();
+showSlidesPack();
+
+function goToContactUsSection() {
+  window.location.href = "#idContactUs";
+}
+
+document
+  .getElementById("idBtnCallToAction")
+  .addEventListener("click", goToContactUsSection);
